7d12fc12cf76c8cd17e6dd043601348c63ed37fb
anonymous <xgqfrms@gmail.com>
2017-01-01 22:40:55 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// main.js
+
+$(function() {
+    $.ajax({
+        url: 'https://cdn.xgqfrms.xyz/json/badges.json',
+        dataType: 'jsonp',
+        success: function(data) {
+            populateWithCourses(data.courses.completed);
+            hide();
+        }
+    });
+    function populateWithCourses(courses) {
+        var $badges = $('#badges');
+        courses.forEach(function(course) {
+            $div = $('<div />', {
+                'class': 'course'
+            }).appendTo($badges);
+            $('<h3 />', {
+                text: course.title
+            }).appendTo($div);
+            $('<img />', {
+                src: course.badge
+            }).appendTo($div);
+            $('<a />', {
+                'class': 'btn btn-primary',
+                target: '_blank',
+                href: course.url,
+                text: 'See Course'
+            }).appendTo($div);
+        });
+    }
+    function hide(){
+        // var hl = document.getElementById('hl');
+        // hl.style.display = "none";
+        var hideLoad = document.getElementById('hideLoad');
+        var state = hideLoad.style.class = "showTure";
+        if (state === "showTure") {
+            state = "hidenTure";
+        } else {
+            //state = "showTure";
+            console.log(`state = "hidenTure"`);
+        }
+    }
+});
+
+/*
+ES 6
+
+selectQueryAll ??? DOM 
+
+let = () => {
+    let = ;
+}
+
+
+
+*/
